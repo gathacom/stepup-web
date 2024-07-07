a6a1ca325c3b980a167813f1f74db706ba019547
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-07-07 19:37:15 +0700

    update about

--- client/src/components/Elements/VideoPlayer.jsx
+++ client/src/components/Elements/VideoPlayer.jsx
@@ -1,7 +1,11 @@
 const VideoPlayer = () => {
   return (
-    <div className="w-full">
-      <img className="w-full h-[250px] rounded-[26px] drop-shadow-xl" src="./images/video-about.png" alt="" />
+    <div className="w-full md:flex md:justify-center lg:justify-end lg:pe-8 lg:items-start">
+      <img
+        className="w-full h-[250px] rounded-[26px] drop-shadow-xl md:w-[552px] md:h-[404px]"
+        src="./images/video-about.png"
+        alt=""
+      />
     </div>
   );
 };
