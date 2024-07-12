aa856444873edd96542278d6507b899b8660feb2
gathacom <gathacom2222@gmail.com>
2024-07-12 22:06:36 +0700

    maintain : pages:beranda, article, konsultasi

--- client/src/pages/konsultasi.jsx
+++ client/src/pages/konsultasi.jsx
@@ -14,28 +14,30 @@ const Konsultasi = () => {
     <div className="pt-16">
       <Navbar user={user} />
       <section className="h-auto">
-        <div className="py-14 bg-head px-6">
-          <SectionHead
-            classname="md:text-start md:text-3xl"
-            content={[
-              { title: "Kami Disini Untuk ", classname: "" },
-              { title: "Membantu ", classname: "text-primary" },
-              { title: "Anda ", classname: "" },
-            ]}
-          />
-          <p className="mt-5 w-full text-xs font-body leading-4 text-dark px-[12px] text-center">
+        <div className="py-14 lg:pt-32 bg-head px-6">
+          <div className="px-[4px] phone375:px-[10px] md:px-44">
+            <SectionHead
+              classname="md:text-start md:text-3xl"
+              content={[
+                { title: "Kami Disini Untuk ", classname: "" },
+                { title: "Membantu ", classname: "text-primary" },
+                { title: "Anda ", classname: "" },
+              ]}
+            />
+          </div>
+          <p className="mt-5 w-full text-xs md:text-base lg:text-lg font-body leading-4 text-dark px-3 md:px-16 lg:px-28 text-center">
             Wujudkan ide brilian Anda dengan bantuan tim kami. kontak kami
             sekarang untuk memulai pencapaian sukses aplikasi Anda! 🚀
           </p>
         </div>
       </section>
       <section className="h-auto">
-        <div className="px-[14px]">
+        <div className="px-[14px] md:px-24 lg:px-[335px]">
           <FormKonsul></FormKonsul>
         </div>
       </section>
       <Footer />
-      <FloatWhatsapp/>
+      <FloatWhatsapp />
     </div>
   );
 };
--- client/src/utils/article.js
+++ client/src/utils/article.js
@@ -1,85 +1,22 @@
-const newArticles = [
-    {
-      id:1,
-      imageCard:"../images/article-image-1.png",
-      title:"Mengenal Apa Itu HTML ?",
-      author:"Cristiano Ronaldo",
-      shortDescription:"Buat kamu yang pernah membuat sebuah website pastinya sudah tidak asing lagi dengan yang namanya HTML. Sebenarnya apa sih HTML itu ? Kenapa menggunakan HTML untuk membuat website?",
-      date:"07 Februari 2024",
-      contentDetail: 
-      [
-        {
-          header: "Apa Itu HTML ?",
-          paragraf1: "Buat kamu yang pernah membuat sebuah website pastinya sudah tidak asing lagi dengan yang namanya HTML. Sebenarnya apa sih HTML itu ? Kenapa menggunakan HTML untuk membuat website ?",
-        },
-        {
-          header: "Pengertian",
-          paragraf1: "HTML merupakan singkatan dari Hyper Text Markup Language, merupakan bahasa markup standard yang digunakan untuk menampilkan dokumen ke Web Browser seperti Google Chrome dan Edge. HTML biasanya digunakan bersama teknologi lainya, seperti Cascading Style Sheets (CSS) dan bahasa scripting",
-          paragraf2: "Struktur di dalam HTML terdiri dari serangkaian elemen, elemen tersebut berfungsi untuk memberi informasi kepada Web Browser tentang cara menampilkan konten. Elemen dalam HTML memiliki banyak jenis dan memiliki fungsinya masing-masing, seperti elemen paragraf , input, judul dan lainya. Pada umumnya elemen terdiri dari tag pembuka, konten dan tag penutup, ada juga sebagian elemen yang tidak memiliki tag penutup dan konten.",
-          paragraf3: "Sedangkan tag itu sendiri ditulis dengan cara diapit tanda kurung sudut dan untuk tag penutup diberi garis miring sebelum nama tagnya, sebagai contoh <p> </p>. Sedangkan untuk elemen yang hanya menggunakan satu tag, cara penulisanya diberi garis miring setelah nama tag ",
-          image:"../images/article-detail-1.png",
-        },
-        {
-          header: "Penutup",
-          paragraf1: "HTML pertama kali dicetus pada tahun 1980 oleh Tim Berners-Lee, pada saat itu dia sedang bekerja di salah satu perusahaan yang bernama CERN (European Organization for Nuclear Research), dia mengusulkan untuk membuat sebuah sistem yang bernama ENQUIRE yang berfungsi untuk mengelola dokumen perusahaan.",
-        },
-        
-      ]
-    },
+const articles = [
+  {
+    ids: "1",
+    image: "./images/article-1.png",
+    published_at: "25 Februari 2024",
+    author: "Ikhsan",
+    title: "The Application of Design Thinking as an UI/UX Designer",
+    content: "User interface and user experience are two very important things in product development.",
+    to:"https://medium.com/@stepup.project/pentingnya-membuat-design-system-95a9064ba50e"
+  },
+  {
+    ids: "2",
+    image: "./images/article-2.png",
+    published_at: "30 Mei 2024",
+    author: "Ikhsan",
+    title: "Mengenal Project Manager",
+    content: "Berbagai sumber menyebutkan bahwa project manager merupakan sebuah posisi yang akan selalu dibutuhkan dalam lingkup proyek, mulai dari skala kecil hingga besar.",
+    to:"https://medium.com/@stepup.project/pentingnya-membuat-design-system-95a9064ba50e"
+  },
 ]
 
-// object card articles
-const articles = [
-    // {
-    //   id:1,
-    //   imageCard:"../images/article-image-1.png",
-    //   title:"Mengenal Apa Itu HTML ?",
-    //   author:"Cristiano Ronaldo",
-    //   shortDescription:"Buat kamu yang pernah membuat sebuah website pastinya sudah tidak asing lagi dengan yang namanya HTML. Sebenarnya apa sih HTML itu ? Kenapa menggunakan HTML untuk membuat website?",
-    //   date:"07 Februari 2024",
-    //   contentDetail: 
-    //   [
-    //     {
-    //       header: "Apa Itu HTML ?",
-    //       paragraf1: "Buat kamu yang pernah membuat sebuah website pastinya sudah tidak asing lagi dengan yang namanya HTML. Sebenarnya apa sih HTML itu ? Kenapa menggunakan HTML untuk membuat website ?",
-    //     },
-    //     {
-    //       header: "Pengertian",
-    //       paragraf1: "HTML merupakan singkatan dari Hyper Text Markup Language, merupakan bahasa markup standard yang digunakan untuk menampilkan dokumen ke Web Browser seperti Google Chrome dan Edge. HTML biasanya digunakan bersama teknologi lainya, seperti Cascading Style Sheets (CSS) dan bahasa scripting",
-    //       paragraf2: "Struktur di dalam HTML terdiri dari serangkaian elemen, elemen tersebut berfungsi untuk memberi informasi kepada Web Browser tentang cara menampilkan konten. Elemen dalam HTML memiliki banyak jenis dan memiliki fungsinya masing-masing, seperti elemen paragraf , input, judul dan lainya. Pada umumnya elemen terdiri dari tag pembuka, konten dan tag penutup, ada juga sebagian elemen yang tidak memiliki tag penutup dan konten.",
-    //       paragraf3: "Sedangkan tag itu sendiri ditulis dengan cara diapit tanda kurung sudut dan untuk tag penutup diberi garis miring sebelum nama tagnya, sebagai contoh <p> </p>. Sedangkan untuk elemen yang hanya menggunakan satu tag, cara penulisanya diberi garis miring setelah nama tag ",
-    //       image:"../images/article-detail-1.png",
-    //     },
-    //     {
-    //       header: "Penutup",
-    //       paragraf1: "HTML pertama kali dicetus pada tahun 1980 oleh Tim Berners-Lee, pada saat itu dia sedang bekerja di salah satu perusahaan yang bernama CERN (European Organization for Nuclear Research), dia mengusulkan untuk membuat sebuah sistem yang bernama ENQUIRE yang berfungsi untuk mengelola dokumen perusahaan.",
-    //     },
-        
-    //   ]
-    // },
-    {
-      id:2,
-      imageCard:"../images/article-image-2.png",
-      title:"Mengenal Apa Itu Artificial Intelegence ?",
-      author:"Cristiano Ronaldo",
-      shortDescription:"Apa itu HTML ? Buat kamu yang pernah membuat sebuah website pastinya sudah tidak...",
-      date:"10 Februari 2024",
-      contentDetail: 
-      [
-        {
-          header: "Apa Itu Ai ?",
-          paragraf1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, dolore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel perferendis molestiae error facilis, itaque tenetur?",
-        },
-        {
-          header: "Pengertian",
-          paragraf1: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse sapiente minus vel ratione ullam reprehenderit doloribus aliquam libero beatae optio praesentium illo, maiores, tempora fuga, similique architecto neque aliquid assumenda? Ea eaque doloremque quibusdam iure dolore vel at obcaecati ad?",
-          image:"./images/article-detail-2.png",
-        },
-        {
-          header: "Penutup",
-          paragraf1: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio nobis fugit a praesentium aspernatur corrupti minima adipisci non et vero.",
-        },
-        
-      ]
-    },
-]
+export default articles;
